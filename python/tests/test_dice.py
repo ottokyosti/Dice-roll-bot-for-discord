ae304a8e6ac29c843355b23702cdbfa0a5c06016
Otto Kyösti (home) <otto.t.kyosti@tuni.fi>
2024-03-07 12:49:19 +0200

    Test class to test validation created

--- python/tests/test_dice.py
+++ python/tests/test_dice.py
@@ -1,23 +1,30 @@
+import re
 import unittest
 from diceFunctions import DiceMachine
 
-class TestDiceFunctions(unittest.TestCase):
+class DiceValidator():
+    @staticmethod
+    def validate(diceStr: str):
+        cleaned_str = diceStr.replace(" ", "")
+        match = re.match(r"(?!.*[\+\-]{2,}|.*d{2,}|.*d[\+\-]|\d*d\d+d|^[\+\-]|.*[d\+\-]$)[\d\+\-d]+", cleaned_str)
 
-    @classmethod
-    def setUpClass(self):
-        self.dm = DiceMachine("d20")
+        if match:
+            return re.findall(r"\d*d\d+|[\+\-]|\d+", cleaned_str)
+        return []
+
+class TestDiceFunctions(unittest.TestCase):
 
     def test_validation(self):
         print("\nTesting validation with valid values:\n")
         print("Does 'd20' get through validation?")
-        print(f"Expected value: ['d20'] | Actual value: {self.dm.validate("d20")}")
-        self.assertEqual(self.dm.validate("d20"), ["d20"])
+        print(f"Expected value: ['d20'] | Actual value: {DiceValidator.validate("d20")}")
+        self.assertEqual(DiceValidator.validate("d20"), ["d20"])
         print("Does '2d8 + 3 - d8' get through validation?")
-        print(f"Expected value: ['2d8', '+', '3', '-', 'd8'] | Actual value: {self.dm.validate("d20")}")
-        self.assertEqual(self.dm.validate("2d8 + 3 - d8"), ["2d8", "+", "3", "-", "d8"])
+        print(f"Expected value: ['2d8', '+', '3', '-', 'd8'] | Actual value: {DiceValidator.validate("d20")}")
+        self.assertEqual(DiceValidator.validate("2d8 + 3 - d8"), ["2d8", "+", "3", "-", "d8"])
         print("Does 'd81+302+43d8' get through validation?")
-        print(f"Expected value: ['d81', '+', '302', '+', '43d8'] | Actual value: {self.dm.validate("d81+302+43d8")}")
-        self.assertEqual(self.dm.validate("d81+302+43d8"), ["d81", "+", "302", "+", "43d8"])
+        print(f"Expected value: ['d81', '+', '302', '+', '43d8'] | Actual value: {DiceValidator.validate("d81+302+43d8")}")
+        self.assertEqual(DiceValidator.validate("d81+302+43d8"), ["d81", "+", "302", "+", "43d8"])
 
         print("\nTesting validation with invalid values:\n")
         test_values = ["2dd2+43", 
@@ -32,8 +39,8 @@ class TestDiceFunctions(unittest.TestCase):
                        "2d"]
         for value in test_values:
             print(f"Does '{value}' fail validation?")
-            print(f"Expected value: [] | Actual value: {self.dm.validate(value)}")
-            self.assertEqual(self.dm.validate(value), [])
+            print(f"Expected value: [] | Actual value: {DiceValidator.validate(value)}")
+            self.assertEqual(DiceValidator.validate(value), [])
         print("Validation testing done")
 
 if __name__ == "__main__":
