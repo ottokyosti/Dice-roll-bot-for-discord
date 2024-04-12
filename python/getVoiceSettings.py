d11d78af9c0d0b1975988da7af39424f60697dbf
Otto Kyösti (home) <otto.t.kyosti@tuni.fi>
2024-04-12 15:18:35 +0300

    getVoiceSettings.py: Add new voice setting

--- python/getVoiceSettings.py
+++ python/getVoiceSettings.py
@@ -3,16 +3,23 @@ from elevenlabs import VoiceSettings
 def get_voice_settings(voice: str):
     if voice == "Roope" or voice == "Otto":
         voice_settings = VoiceSettings(
-                stability = 0.3,
-                similarity_boost = 0.9,
-                style = 0.75,
-                use_speaker_boost = True
-            )
+            stability = 0.3,
+            similarity_boost = 0.9,
+            style = 0.75,
+            use_speaker_boost = True
+        )
     elif voice == "Pappa" or voice == "Nipsu":
         voice_settings = VoiceSettings(
             stability = 0.5,
             similarity_boost = 0.9,
             style = 0.25,
+            use_speaker_boost = True
+        )
+    elif voice == "Niilo22":
+        voice_settings = VoiceSettings(
+            stability = 0.3,
+            similarity_boost = 0.9,
+            style = 0.5,
             use_speaker_boost = True
         )
     else:
