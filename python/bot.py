ed2ef4c3ef26a0b04888de42ac5746cca58f0ad0
Otto Kyösti (home) <otto.t.kyosti@tuni.fi>
2025-02-06 21:10:51 +0200

    add volume parameter to play commandand add stop command

--- python/bot.py
+++ python/bot.py
@@ -176,7 +176,7 @@ async def chipi(ctx: commands.Context):
         await ctx.send("You must be in a voice channel to use this command!")
 
 @bot.hybrid_command(name = "play", description = "Play a sound bite from Youtube")
-async def play(ctx: commands.Context, url: str):
+async def play(ctx: commands.Context, url: str, volume: int = 50):
     if ctx.author.voice and ctx.author.voice.channel:
         await ctx.defer()
         voice_channel = ctx.author.voice.channel
@@ -193,26 +193,39 @@ async def play(ctx: commands.Context, url: str):
         }
 
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info(url, download = False)
-            duration = info.get("duration", 0)
-            if (duration > 60):
-                await ctx.send("Video is too long! Must be under 60 seconds")
-                return
-            
             info = ydl.extract_info(url, download = True)
+            duration = info.get("duration", 0)
             filename = ydl.prepare_filename(info).replace(".webm", ".mp3").replace(".m4a", ".mp3")
 
-        voice_client = await voice_channel.connect()
-        if not voice_client.is_playing():
-            voice_client.play(FFmpegPCMAudio(filename))
-            while voice_client.is_playing():
-                await asyncio.sleep(0.5)
+        if (duration < 60):
+            voice_client = await voice_channel.connect()
+            if not voice_client.is_playing():
+                await ctx.send(f"Playing audio at {volume}% volume")
+                ffmpeg_options = {
+                    "options": f"-filter:a 'volume={volume / 100}'"
+                }
+                voice_client.play(FFmpegPCMAudio(filename, **ffmpeg_options))
+                while voice_client.is_playing():
+                    await asyncio.sleep(0.5)
+            else:
+                await ctx.send("I'm already playing!")
+            await voice_client.disconnect()
+            await ctx.send(url)
         else:
-            await ctx.send("I'm already playing!")
-        await voice_client.disconnect()
-        await ctx.send(url)
+            await ctx.send("Video is too long! Must be under 60 seconds")
         os.remove(filename)
     else:
         await ctx.send("You must be in a voice channel to use this command!")
+
+@bot.hybrid_command(name = "stop", description = "Stop the bot from playing audio")
+async def stop(ctx: commands.Context):
+    if (ctx.voice_client):
+        if (ctx.voice_client.is_playing()):
+            ctx.voice_client.stop()
+            await ctx.send("Stopped playing audio")
+        else:
+            await ctx.send("I'm not playing audio...")
+    else:
+        await ctx.send("I'm not connected to a voice channel...")
 
 bot.run(token)
